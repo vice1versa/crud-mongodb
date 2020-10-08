ce7c8b0e346152f5e4ef3fa68b31cfeb2963d500
vice1versa <sebastian.bernhard97@gmail.com>
2020-10-08 16:30:25 +0200

    added db connection class

---
+++ src/main/java/com/mongodb/quickstart/Connection.java
@@ -0,0 +1,25 @@
+package com.mongodb.quickstart;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Connection {
+
+	public static void main(String[] args) {
+		
+		Logger.getLogger("org.mongodb.driver").setLevel(Level.WARNING);
+		String connectionString = System.getProperty("mongodb.uri");
+		
+		try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+			
+			List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
+			databases.forEach(db -> System.out.println(db.toJson()));
+		}
+	}
+}
