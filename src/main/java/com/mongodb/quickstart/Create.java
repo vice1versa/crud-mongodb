cdb7c695ca2efaa30801119530f922676b0e6248
vice1versa <sebastian.bernhard97@gmail.com>
2020-10-08 16:39:56 +0200

    create bson document

--- src/main/java/com/mongodb/quickstart/Create.java
+++ src/main/java/com/mongodb/quickstart/Create.java
@@ -5,7 +5,9 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,6 +21,17 @@ public class Create {
 
             MongoDatabase sampleTrainingDB = mongoClient.getDatabase("sample_training");
             MongoCollection<Document> gradesCollection = sampleTrainingDB.getCollection("grades");
+            
+            Random rand = new Random();
+            Document student = new Document("_id", new ObjectId());
+            student.append("student_id", 10000d)
+                   .append("class_id", 1d)
+                   .append("scores", asList(new Document("type", "exam").append("score", rand.nextDouble() * 100),
+                                            new Document("type", "quiz").append("score", rand.nextDouble() * 100),
+                                            new Document("type", "homework").append("score", rand.nextDouble() * 100),
+                                            new Document("type", "homework").append("score", rand.nextDouble() * 100)));
+
+            gradesCollection.insertOne(student);
         }
 	}
 }
